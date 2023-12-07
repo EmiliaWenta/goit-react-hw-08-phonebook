060e91003fab1db8c559b9a6309dea2c8798cd1c
Emilia Wenta <emiliakielinska96@gmail.com>
2023-12-07 19:38:31 +0100

    Add Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,42 +1,110 @@
-import { Helmet } from 'react-helmet';
-
-// import React, { useEffect, useState } from 'react';
-// import { getTrendingMovies } from '../Api/getTrendingMovies';
-// import { Link } from 'react-router-dom';
-
-// import css from './Home.module.css';
-
-// export default function Home() {
-//   const [movies, setMovies] = useState([]);
-
-//   useEffect(() => {
-//     async function fetchMovies() {
-//       const movies = await getTrendingMovies();
-//       setMovies([...movies]);
-//     }
-
-//     fetchMovies();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1 className={css.trendingMovies__title}>Trending Today</h1>
-//       <ul className={css.trendingMovies__list}>
-//         {movies?.map(movie => (
-//           <li className={css.trendingMovies__listItem} key={movie.id}>
-//             <Link
-//               state={{ from: '/', id: `${movie.id}` }}
-//               to={`/movies/${movie.id}`}
-//             >
-//               {movie.title}
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Image,
+  Stack,
+  Button,
+  SimpleGrid,
+} from '@chakra-ui/react';
+import {
+  CheckIcon,
+  EditIcon,
+  SearchIcon,
+  DeleteIcon,
+  PhoneIcon,
+} from '@chakra-ui/icons';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Card
+        direction={{ base: 'column', sm: 'row' }}
+        alignItems="center"
+        variant="outline"
+        padding="20px"
+      >
+        <CheckIcon />
+
+        <Stack>
+          <CardBody>
+            <Heading size="md">
+              Introducing "PhoneBookSave" – Your Ultimate Contact App!
+            </Heading>
+
+            <Text py="2">
+              Tired of losing track of important contacts? Meet PhoneBookSave,
+              the innovative app designed to revolutionize how you organize your
+              contacts effortlessly.
+            </Text>
+          </CardBody>
+        </Stack>
+      </Card>
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Intuitive Interface:</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Streamlined design for seamless navigation and user-friendly
+              experience.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <EditIcon />
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Quick Search:</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Find any contact in seconds with our powerful search feature. No
+              more scrolling endlessly.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <SearchIcon />
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Easy Deletion: </Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Need to declutter your contacts? PhoneBookSave makes it a breeze
+              to remove unwanted entries. Just a one taps, and you're done!
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <DeleteIcon />
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">24/7 Accessibility: </Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Whether you're at work, home, or on the go, ConnectSave is at your
+              fingertips. Access your contacts anytime, anywhere – our app is
+              designed to keep you connected on your terms.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <PhoneIcon />
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
+    </>
+  );
 }
